9621a2b3d5a53fe2ed4ca42c4f9ba014cea4d7fb
Durgawad <sdurgawad@sharadd.pp-devcos-fl-team.us-central1.gcp.dev.paypalinc.com>
2017-12-23 05:18:45 -0800

    Added the code for sphere and cube

--- area_calc.py
+++ area_calc.py
@@ -12,7 +12,7 @@ def Area_Circle(r):
 
 print("\n Enter the list of Shapes: ")
 
-print("\n 1. Triangle \n 2. Rectangle \n 3. Square \n 4. Circle \n 5. Quit \n")
+print("\n 1. Triangle \n 2. Rectangle \n 3. Square \n 4. Circle \n 5. Cube \n 6. Sphere \n 7. Quit \n")
 
 option = int(input('Enter the option: '))
 
@@ -38,4 +38,14 @@ elif (option == 4):
     print("\n Area of Circle = %f" % Area_Circle(r))
 
 elif (option == 5):
+    r = int(input('Enter the side: '))
+    area = float(6*r*r)
+    print("\n Area of Cube = %f" % area)
+
+elif (option == 6):
+    r = int(input('Enter the radius: '))
+    area = float(4 * r * r * math.pi)
+    print("\n Area of Sphere = %f" % area)
+
+elif (option == 7):
     exit()
